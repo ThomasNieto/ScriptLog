01892528f9e3003c3897697d06e4f3e32d73ff7d
Thomas Nieto <38873752+tnieto88@users.noreply.github.com>
2019-10-31 21:41:08 -0500

    Add ToString method

--- src/ScriptLog/Classes/LogMessage.ps1
+++ src/ScriptLog/Classes/LogMessage.ps1
@@ -1,6 +1,13 @@
+using namespace System.Collections
+using module ScriptLog
+
 class LogMessage {
     [datetime]$TimeStamp
     [string]$Message
     [LogLevel]$Level
     [IDictionary]$Invocation
+
+    [string] ToString() {
+        return $this.Message
+    }
 }
