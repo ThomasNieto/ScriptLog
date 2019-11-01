8dc60618d38e6cc2a8c849ad0e3719accf392af1
Thomas Nieto <38873752+tnieto88@users.noreply.github.com>
2019-10-31 21:45:04 -0500

    Add script log parser

--- src/ScriptLog/Private/ConvertFrom-ScriptLog.ps1
+++ src/ScriptLog/Private/ConvertFrom-ScriptLog.ps1
@@ -17,7 +17,45 @@ function ConvertFrom-ScriptLog {
     }
     
     process {
+        $scriptInfo = @{}
+        $messageCache = @()
         
+        foreach ($line in $String) {
+            if ($line -match '^w+ = ') {
+                if ($line -match '^(Property>\w+) = (?Value>.+)$') {
+                    $scriptProperty = @{ $Matches.Property = $Matches.Value }
+                }
+                elseif ($line -match '^(Property>\w+) = $') {
+                    $scriptProperty = $null
+                }
+
+                if ($scriptProperty['StartTime']) {
+                    if ($messageCache) {
+                        $scriptInfo = ConvertFrom-StringHashtable -Hashtable $scriptInfo
+                    }
+
+                    #TODO: Add invocation to all messages
+
+                    $scriptInfo = @{}
+                    $scriptInfo += $scriptProperty
+                }
+                elseif ($scriptProperty['EndTime']) {
+                    $scriptInfo += $scriptProperty
+                    
+                    if ($messageCache) {
+                        $scriptInfo = ConvertFrom-StringHastable -Hashtable $scriptInfo
+    
+                        #TODO: Add invocation to all messages
+                    }
+                }
+                else {
+                    $scriptInfo += $scriptProperty
+                }
+
+                
+            }
+            
+        }
     }
     
     end {
