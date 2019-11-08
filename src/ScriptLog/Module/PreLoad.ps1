2cb8c853c94347f056a4ccdafd516ef73ed1636f
Thomas Nieto <38873752+tnieto88@users.noreply.github.com>
2019-11-07 20:04:41 -0600

    Update variable to be constant

--- src/ScriptLog/Module/PreLoad.ps1
+++ src/ScriptLog/Module/PreLoad.ps1
@@ -5,4 +5,4 @@ Set-StrictMode -Version 2
 
 Import-LocalizedData -BindingVariable LocalizedData -FileName ScriptLog.Resources.psd1
 
-$script:DATETIMEFORMAT = 'o'
+New-Variable -Name DATETIMEFORMAT -Value 'o' -Option Constant -Scope Script
