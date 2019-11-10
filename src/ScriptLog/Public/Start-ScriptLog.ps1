cdb8aa09f05326d29ad33aee7c1848338b283c9c
Thomas Nieto <38873752+tnieto88@users.noreply.github.com>
2019-11-09 23:38:00 -0600

    Add various enhancements

--- src/ScriptLog/Public/Start-ScriptLog.ps1
+++ src/ScriptLog/Public/Start-ScriptLog.ps1
@@ -3,7 +3,7 @@ function Start-ScriptLog {
     [OutputType([PSObject])]
     param (
         [Parameter(
-            Mandatory,
+            ParameterSetName = 'Path',
             Position = 0
         )]
         [ValidateNotNullOrEmpty()]
@@ -11,31 +11,17 @@ function Start-ScriptLog {
         $Path,
 
         [Parameter(
-            ParameterSetName = 'ScriptInfo',
+            ParameterSetName = 'OutputDirectory',
             Position = 0
         )]
         [ValidateNotNullOrEmpty()]
+        [string]
+        $OutputDirectory,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
         [PSObject]
         $ScriptInfo,
-
-        [Parameter(
-            ParameterSetName = 'Path'
-        )]
-        [ValidateNotNullOrEmpty()]
-        [string]
-        $ScriptName,
-
-        [Parameter(
-            ParameterSetName = 'Path'
-        )]
-        [version]
-        $ScriptVersion,
-
-        [Parameter(
-            ParameterSetName = 'Path'
-        )]
-        [string]
-        $ScriptPath,
 
         [Parameter()]
         [System.Collections.IDictionary]
@@ -45,32 +31,43 @@ function Start-ScriptLog {
         [switch]
         $NoEnvironmentInfo,
 
-        [Parameter(
-            ParameterSetName = 'ScriptInfo'
-        )]
-        [switch]
-        $NoScriptInfo,
-
         [Parameter()]
         [switch]
         $NoClobber,
 
         [Parameter()]
-        [int]
-        $Retention,
-
-        [Parameter()]
-        [long]
-        $MaximumFileSize,
+        [switch]
+        $Append,
 
         [Parameter()]
         [switch]
         $PassThru
     )
+
+    if (-not $PSBoundParameters['Path']) {
+        # A random string is used to match Start-Transcript default file name
+        $randomBytes = New-Object -TypeName Byte[] -ArgumentList 6
+        [System.Security.Cryptography.RandomNumberGenerator]::Create().GetBytes($randomBytes)
+        $randomString = [convert]::ToBase64String($randomBytes) -replace '/', '_'
+
+        if ($PSBoundParameters['ScriptInfo']) {
+            $fileName = $LocalizedData.DefaultScriptFullName -f $ScriptInfo.Name, [Environment]::MachineName, $randomString, (Get-Date)
+        }
+        else {
+            $fileName = $LocalizedData.DefaultScriptFullName -f $LocalizedData.DefaultScriptName, [Environment]::MachineName, $randomString, (Get-Date)
+        }
+
+        if ($PSBoundParameters['OutputDirectory']) {
+            $Path = Join-Path -Path $OutputDirectory -ChildPath $fileName
+        }
+        else {
+            $Path = Join-Path -Path ([Environment]::GetFolderPath('MyDocuments')) -ChildPath $fileName
+        }
+    }
     
-    if ($NoClobber -and (Test-Path -Path $Path)) {
+    if ($NoClobber -and -not $Append -and (Test-Path -Path $Path)) {
         $paramWriteError = @{
-            Message      = $LocalizedData.FileExistsNoClobber
+            Message      = ($LocalizedData.FileExistsNoClobber -f $Path)
             Category     = 'ResourceExists'
             TargetObject = $Path
         }
@@ -79,42 +76,52 @@ function Start-ScriptLog {
     }
 
     $header = @()
-    $header += Get-Padding -String $LocalizedData.ScriptLogStartHeader -PaddingCharacter $LocalizedData.DividerCharaceter
+    $header += Get-Padding -String $LocalizedData.ScriptLogStartHeader -PaddingCharacter $LocalizedData.DividerCharacter
     
-    $invocation = @{}
+    $invocation = @{ }
     $invocation['StartTime'] = Get-Date
     $invocation['UserName'] = '{0}\{1}' -f [Environment]::UserDomainName, [Environment]::UserName
     $invocation['Machine'] = [Environment]::MachineName
     $invocation['ProcessId'] = $PID
-
+    
     foreach ($item in $invocation.GetEnumerator()) {
-        $header += '{0} = {1}' -f $item.Key, $item.Value
+        if ($item.Key -eq 'StartTime' -or $item.Key -eq 'EndTime') {
+            $value = $item.Value.ToString($script:DATETIMEFORMAT)
+        }
+        else {
+            $value = $item.Value
+        }
+
+        $header += '{0} = {1}' -f $item.Key, $Value
     }
+
+    $invocation['Host'] += $Host.Name
+    $invocation += $PSVersionTable
 
     if (-not $NoEnvironmentInfo) {
         $header += Get-Padding -String $LocalizedData.EnvironmentHeader
+        $header += '{0} = {1}' -f 'Host', $Host.Name
         
         foreach ($item in $PSVersionTable.GetEnumerator()) {
-            #TODO: Fix array values
-            $header += '{0} = {1}' -f $item.Key, $item.Value
+            $header += '{0} = {1}' -f $item.Key, ($item.Value -join ', ')
         }
     }
 
-    if ($PSCmdlet.ParameterSetName -eq 'ScriptInfo' -and -not $NoScriptInfo -or $ScriptName -or $ScriptVersion -or $ScriptPath) {
+    if ($PSBoundParameters['ScriptInfo'] -or $PSBoundParameters['BoundParameters']) {
         $header += Get-Padding -String $LocalizedData.ScriptInvocationHeader
     }
 
-    if ($PSCmdlet.ParameterSetName -eq 'ScriptInfo') {
+    if ($PSBoundParameters['ScriptInfo']) {
         $invocation['ScriptName'] = $ScriptInfo.Name
         $invocation['ScriptVersion'] = $ScriptInfo.Version
         $invocation['ScriptPath'] = $ScriptInfo.Path
-
-        $header += 'ScriptName = {0}' -f $ScriptInfo.Name
-        $header += 'ScriptVersion = {0}' -f $ScriptInfo.Version
-        $header += 'ScriptPath = {0}' -f $ScriptInfo.Path
+        
+        $header += 'ScriptName = {0}' -f $ScriptName
+        $header += 'ScriptVersion = {0}' -f $ScriptVersion
+        $header += 'ScriptPath = {0}' -f $ScriptPath
     }
 
-    if ($BoundParameters) {
+    if ($PSBoundParameters['BoundParameters']) {
         foreach ($item in $BoundParameters.GetEnumerator()) {
             $key = 'ScriptParameter_{0}' -f $item.Key
             $invocation[$key] = $item.Value
@@ -123,18 +130,19 @@ function Start-ScriptLog {
         }
     }
 
-    $header += $LocalizedData.DividerCharaceter * $LocalizedData.DividerLength
+    $header += $LocalizedData.DividerCharacter * $LocalizedData.DividerLength
 
-    Add-Content -Value $header -Path $Path
-
-    $invocation += $PSVersionTable
+    if ($Append) {
+        Add-Content -Value $header -Path $Path
+    }
+    else {
+        Set-Content -Value $header -Path $Path
+    }
 
     if ($PassThru) {
         $output = [ScriptLogInfo]@{
-            Path            = $Path
-            Retention       = $Retention
-            MaximumFileSize = $MaximumFileSize
-            Invocation      = $invocation
+            Path       = $Path
+            Invocation = $invocation
         }
 
         Write-Output -InputObject $output
