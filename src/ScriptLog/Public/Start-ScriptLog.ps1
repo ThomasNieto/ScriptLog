571455170daff5aa6accfc27ffad13ebb2eab95b
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2019-11-19 20:33:26 -0600

    Fix writing properties to log file

--- src/ScriptLog/Public/Start-ScriptLog.ps1
+++ src/ScriptLog/Public/Start-ScriptLog.ps1
@@ -94,15 +94,15 @@ function Start-ScriptLog {
     $header = @()
     $header += Get-Padding -String $LocalizedData.ScriptLogStartHeader
 
-    foreach ($item in $scriptLogInfo.PSEnvironment.GetEnumerator()) {
-        if ($item.Key -eq 'StartTime' -or $item.Key -eq 'EndTime') {
-            $value = $item.Value.ToString($script:DATETIMEFORMAT)
+    foreach ($property in 'StartTime', 'UserName', 'ComputerName', 'ProcessId') {
+        if ($property -eq 'StartTime' -or $property -eq 'EndTime') {
+            $value = $scriptLogInfo.$property.ToString($script:DATETIMEFORMAT)
         }
         else {
-            $value = $item.Value
+            $value = $scriptLogInfo.$property
         }
 
-        $header += '{0} = {1}' -f $item.Key, $Value
+        $header += '{0} = {1}' -f $property, $Value
     }
 
     if (-not $NoEnvironmentInfo) {
@@ -131,8 +131,6 @@ function Start-ScriptLog {
     if ($PSBoundParameters['BoundParameters']) {
         foreach ($item in $scriptLogInfo.BoundParameters.GetEnumerator()) {
             $key = 'ScriptParameter_{0}' -f $item.Key
-            $invocation[$key] = $item.Value
-
             $header += '{0} = {1}' -f $key, $item.Value
         }
     }
